342438e23db5fa7e94d4ab5a6d09bc8d07082e63
Alazar Bekele <alazarthe70@gmail.com>
2026-05-30 16:52:20 +0300

    feat: finish login design, auth login google, github

--- frontend/vite-project/src/Components/Sidebar/sidebar.jsx
+++ frontend/vite-project/src/Components/Sidebar/sidebar.jsx
@@ -1,6 +1,4 @@
-import { LuMessageCircleMore } from "react-icons/lu";
-import { FaPlus } from "react-icons/fa6";
-import { IoSearchOutline } from "react-icons/io5";
+import { Icons } from "../../assets/Icons/Icons.jsx";
 
 function Sidebar() {
   return (
@@ -8,17 +6,17 @@ function Sidebar() {
       <div className="w-[20%] h-screen border-r border-gray-200 p-4">
         <div className="w-full size-18 rounded-full bg-radial-[at_25%_25%] from-indigo-400 to-indigo-600 to-75% rounded-full flex justify-evenly items-center">
           <div className="w-2/3 ps-4 flex justify-start items-center gap-1">
-            <LuMessageCircleMore size={25} className="text-white" />
+            <Icons.message size={25} className="text-white" />
             <p className="font-bold text-white">Alazar.</p>
           </div>
 
           <div className="flex justify-center items-center hover:rotate-45 transition delay-100 duration-300 ease-in-out">
-            <FaPlus size={50} className="w-1/3 text-white" />
+            <Icons.plus size={50} className="w-1/3 text-white" />
           </div>
         </div>
 
         <div className="p-2 relative">
-          <IoSearchOutline className="text-gray-500 absolute left-8 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          <Icons.search className="text-gray-500 absolute left-8 top-1/2 -translate-x-1/2 -translate-y-1/2" />
           <input
             className="ps-10 bg-indigo-100 w-full p-2 rounded-lg outline-none"
             type="text"
