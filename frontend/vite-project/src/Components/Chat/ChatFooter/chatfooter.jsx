f96a067dc0ead1b3540da5603f1c614ddaa881e8
Alazar Bekele <alazarthe70@gmail.com>
2026-05-31 22:56:42 +0300

    feat: finish footer copyright adding

--- frontend/vite-project/src/Components/Chat/ChatFooter/chatfooter.jsx
+++ frontend/vite-project/src/Components/Chat/ChatFooter/chatfooter.jsx
@@ -1,8 +1,10 @@
 function Chatfooter() {
   return (
     <>
-      <div className="w-full h-[5vh]">
-        <h1>Footer</h1>
+      <div className="w-full h-[4vh] flex justify-center items-center">
+        <h1 className="text-xs text-gray-400 font-extralight">
+          © 2025 NexChat. <span className="font-bold">Alazar Bekele</span>
+        </h1>
       </div>
     </>
   );
