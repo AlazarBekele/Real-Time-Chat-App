e6c9ffb2bcb6797dfc68379644eb54705a86a5ac
Alazar Bekele <alazarthe70@gmail.com>
2026-06-01 13:46:34 +0300

    feat: complate the chat body flex, alignments, date and time

--- frontend/vite-project/src/Components/Chat/ChatBody/chatbody.jsx
+++ frontend/vite-project/src/Components/Chat/ChatBody/chatbody.jsx
@@ -1,8 +1,196 @@
 function Chatbody() {
   return (
     <>
-      <div className="w-full h-[72vh] p-6">
-        <h1>Hello</h1>
+      <div className="w-full h-[72vh] overflow-auto p-6">
+        <div className="w-full h-full flex flex-col justify-end itmes-end gap-6">
+          <div className="w-full pt-8 flex flex-col overflow-auto">
+            <div className="w-full h-[30px] flex justify-center items-end">
+              <p className="bg-gray-300 p-2 w-[100px] text-center text-gray-500 rounded-full">
+                Today
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-start items-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam sit sunt obcaecati labore amet distinctio possimus
+                in, accusamus neque velit a minus ipsum unde doloremque soluta
+                repellendus, cumque explicabo debitis!
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start itmes-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-white text-black border border-gray-400 px-4 py-2 rounded-tl-lg rounded-br-lg rounded-tr-lg break-words">
+                hi! Alazar Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Dolor possimus ipsum minima culpa quasi! Nostrum ipsum
+                reprehenderit quisquam cum ex, iure aperiam ducimus aliquam
+                unde! Fuga quod et facere est.
+                <div className="w-full flex justify-end">
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-white text-black border border-gray-400 px-4 py-2 rounded-tl-lg rounded-br-lg rounded-tr-lg break-words">
+                hi! Alazar Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Magni, molestiae corrupti numquam eum at deleniti voluptas
+                quibusdam quia minima corporis fugiat mollitia natus neque et
+                blanditiis tenetur porro ducimus quaerat?
+                <div className="w-full flex justify-end">
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start items-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam sit sunt obcaecati labore amet distinctio possimus
+                in, accusamus neque velit a minus ipsum unde doloremque soluta
+                repellendus, cumque explicabo debitis!
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start items-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam sit sunt obcaecati labore amet distinctio possimus
+                in, accusamus neque velit a minus ipsum unde doloremque soluta
+                repellendus, cumque explicabo debitis!
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start itmes-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-white text-black border border-gray-400 px-4 py-2 rounded-tl-lg rounded-br-lg rounded-tr-lg break-words">
+                hi! Alazar Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Dolor possimus ipsum minima culpa quasi! Nostrum ipsum
+                reprehenderit quisquam cum ex, iure aperiam ducimus aliquam
+                unde! Fuga quod et facere est.
+                <div className="w-full flex justify-end">
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-white text-black border border-gray-400 px-4 py-2 rounded-tl-lg rounded-br-lg rounded-tr-lg break-words">
+                hi! Alazar Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Magni, molestiae corrupti numquam eum at deleniti voluptas
+                quibusdam quia minima corporis fugiat mollitia natus neque et
+                blanditiis tenetur porro ducimus quaerat?
+                <div className="w-full flex justify-end">
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start items-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam sit sunt obcaecati labore amet distinctio possimus
+                in, accusamus neque velit a minus ipsum unde doloremque soluta
+                repellendus, cumque explicabo debitis!
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start items-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam sit sunt obcaecati labore amet distinctio possimus
+                in, accusamus neque velit a minus ipsum unde doloremque soluta
+                repellendus, cumque explicabo debitis!
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start itmes-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-white text-black border border-gray-400 px-4 py-2 rounded-tl-lg rounded-br-lg rounded-tr-lg break-words">
+                hi! Alazar Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Dolor possimus ipsum minima culpa quasi! Nostrum ipsum
+                reprehenderit quisquam cum ex, iure aperiam ducimus aliquam
+                unde! Fuga quod et facere est.
+                <div className="w-full flex justify-end">
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-white text-black border border-gray-400 px-4 py-2 rounded-tl-lg rounded-br-lg rounded-tr-lg break-words">
+                hi! Alazar Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Magni, molestiae corrupti numquam eum at deleniti voluptas
+                quibusdam quia minima corporis fugiat mollitia natus neque et
+                blanditiis tenetur porro ducimus quaerat?
+                <div className="w-full flex justify-end">
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-start items-end gap-2 pt-6">
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+
+              <div className="w-fit max-w-[70%] bg-purple-500 text-white px-4 py-2 rounded-tl-lg rounded-bl-lg rounded-tr-lg break-words">
+                hi! Larry Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Numquam sit sunt obcaecati labore amet distinctio possimus
+                in, accusamus neque velit a minus ipsum unde doloremque soluta
+                repellendus, cumque explicabo debitis!
+                <div>
+                  <p className="font-extralight text-sm">8:00 PM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
